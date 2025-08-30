bc7af3bfe77b3f0bd9651ae1bffd8088c086db69
cookiemiro <xodml9598@gmail.com>
2025-08-30 22:18:44 +0900

    config: alembic 설정 추가 및 스키마 업데이트

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,9 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.models import Base  # 여기가 중요!
+from app.database import DATABASE_URL
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -14,11 +17,13 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata  # 모델 메타데이터 지정
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -38,7 +43,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option("sqlalchemy.url", DATABASE_URL)
     context.configure(
         url=url,
         target_metadata=target_metadata,
